e596f7954bb8f06f03b4a21b66f6965db058ca30
Jeronimo <Diazjeronimo70@gmail.com>
2025-04-20 14:07:55 -0500

    a

--- evento.js
+++ evento.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const current = window.location.pathname.split("/").pop();
+    console.log("Ruta actual:", window.location.pathname);
     document.querySelectorAll("nav a").forEach(link => {
       if (link.getAttribute("href") === current) {
         link.classList.add("activo");
